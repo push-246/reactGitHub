42b86e032ca3d97d6b3ec2c0dbced3e439be9189
pushpa1pull <42231236+pushpa1pull@users.noreply.github.com>
2023-03-01 17:21:27 +0530

    first_commit

---
+++ REACT_FIRST_APP/src/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar =()=>{
+    return<>
+        <ul style={{display: "flex",gap:"30px",listStyle: "none",alignItems:"centre"}}>
+            <li>About</li>
+            <li>Contact Us</li>
+            <li>Shop</li>
+        </ul>
+    </>
+}
+
+export default Navbar
