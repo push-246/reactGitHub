cb1f53d01e4582fbc760c240c641c9762ab8996b
pushpa1pull <42231236+pushpa1pull@users.noreply.github.com>
2023-03-05 23:54:32 +0530

    creating root files for scratch

---
+++ 01_03_2023_REACT_FIRST_APP/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import Navbar from "./NavBar"
+
+export const App = ()=>{
+    return(
+        <>
+            <Navbar />
+            <h1>hello!!!!!</h1>
+        </>
+    )
+}
